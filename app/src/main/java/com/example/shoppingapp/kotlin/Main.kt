bd894559f0b3e537e1a6a9f1255315b004b82c18
gaeun <geun5744@gmail.com>
2023-08-17 19:54:48 +0900

    [FEAT] 실습 3-1. main 함수의 구현을 클래스의 함수로 분리

---
+++ app/src/main/java/com/example/shoppingapp/kotlin/Main.kt
@@ -0,0 +1,8 @@
+package com.example.shoppingapp.kotlin
+
+import com.example.shoppingapp.kotlin.screen.ShoppingHome
+
+fun main(args: Array<String>) {
+    val shoppingHome = ShoppingHome()
+    shoppingHome.start()
+}
