23084a4d9e1a98b8d624a0863c2e5cf824f91e59
gaeun <geun5744@gmail.com>
2023-08-29 10:40:19 +0900

    [FEAT] Navigation Component로 화면 이동 구성

--- app/src/main/java/com/example/shoppingapp/HomeFragment.kt
+++ app/src/main/java/com/example/shoppingapp/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class HomeFragment : Fragment() {
 
@@ -20,11 +21,12 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        parentFragment
+        childFragmentManager
+
         val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
         button.setOnClickListener {
-            val transaction = parentFragmentManager.beginTransaction()
-            transaction.add(R.id.container_main, ProductDetailFragment())
-            transaction.commit()
+            findNavController().navigate(R.id.action_home_to_product_detail)
         }
     }
 }
