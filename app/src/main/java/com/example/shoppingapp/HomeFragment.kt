2a2c51fe6f0f8cd734b9eafd6e3ad73d32ea6611
gaeun <geun5744@gmail.com>
2023-09-01 23:20:18 +0900

    [FEAT] viewpager 다음 페이지 미리 앞당긴 UI 구현

--- app/src/main/java/com/example/shoppingapp/HomeFragment.kt
+++ app/src/main/java/com/example/shoppingapp/HomeFragment.kt
@@ -10,7 +10,8 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
-import org.json.JSONObject
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.gson.Gson
 
 class HomeFragment : Fragment() {
 
@@ -31,33 +32,36 @@ class HomeFragment : Fragment() {
         val viewpagerIndicator = view.findViewById<TabLayout>(R.id.viewpager_home_banner_indicator)
 
         val assetLoader = AssetLoader()
-        val homeData = assetLoader.getJsonString(requireContext(), "home.json")
-        Log.d("homeData", homeData ?: "")
+        val homeJsonString = assetLoader.getJsonString(requireContext(), "home.json")
+        Log.d("homeData", homeJsonString ?: "")
 
-        if (!homeData.isNullOrEmpty()) {
-            val jsonObject = JSONObject(homeData)
-            val title = jsonObject.getJSONObject("title")
-            val text = title.getString("text")
-            val iconUrl = title.getString("icon_url")
-            val titleValue = Title(text, iconUrl)
+        if (!homeJsonString.isNullOrEmpty()) {
+            val gson = Gson()
+            val homeData = gson.fromJson(homeJsonString, HomeData::class.java)
 
-            toolbarTitle.text = titleValue.text
-            GlideApp.with(this).load(titleValue.iconUrl).into(toolbarIcon)
+            toolbarTitle.text = homeData.title.text
+            GlideApp.with(this).load(homeData.title.iconUrl).into(toolbarIcon)
 
-            val topBanners = jsonObject.getJSONArray("top_banners")
-            val size = topBanners.length()
-            for (index in 0 until size) {
-                val bannerObject = topBanners.getJSONObject(index)
-                val backgroundImageUrl = bannerObject.getString("background_image_url")
-                val badgeObject = bannerObject.getJSONObject("badge")
-                val badgeLabel = badgeObject.getString("label")
-                val badgeBackgroundColor = badgeObject.getString("background_color")
-                val bannerBadge = BannerBadge(badgeLabel, badgeBackgroundColor)
+            viewpager.adapter = HomeBannerAdapter().apply {
+                submitList(homeData.topBanners)
             }
 
-//            viewpager.adapter = HomeBannerAdapter().apply {
-//                submitList()
-//            }
+            val pageWidth = resources.getDimension(R.dimen.viewpager_item_width)
+            val pageMargin = resources.getDimension(R.dimen.viewoager_item_margin)
+            val screenWidth = resources.displayMetrics.widthPixels
+            val offset = screenWidth - pageWidth - pageMargin
+
+            viewpager.offscreenPageLimit = 3
+
+            viewpager.setPageTransformer { page, position ->
+                page.translationX = position * -offset
+            }
+
+            TabLayoutMediator(
+                viewpagerIndicator,
+                viewpager,
+            ) { tab, position ->
+            }.attach()
         }
     }
 }
