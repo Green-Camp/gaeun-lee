d7a7f63b93f2385c81fc123a7d2519d1ea50dca1
gaeun <geun5744@gmail.com>
2023-09-10 23:55:41 +0900

    [FIX] 타입 오류 수정

--- app/src/main/java/com/example/shoppingapp/ui/common/ImageBindingAdapters.kt
+++ app/src/main/java/com/example/shoppingapp/ui/common/ImageBindingAdapters.kt
@@ -5,7 +5,7 @@ import androidx.databinding.BindingAdapter
 import com.example.shoppingapp.GlideApp
 
 @BindingAdapter("imageUrl")
-fun loadImage(view: ImageView, imageUrl: String) {
+fun loadImage(view: ImageView, imageUrl: String?) {
     if (!imageUrl.isNullOrEmpty()) {
         GlideApp.with(view).load(imageUrl).into(view)
     }
