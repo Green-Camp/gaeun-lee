65a4581f84de806930339ee7b9285d5864c60993
gaeun <geun5744@gmail.com>
2023-09-14 14:19:07 +0900

    [FEAT] 카테고리 목록 adapter 구현 및 dataBinding 적용

--- app/src/main/java/com/example/shoppingapp/ui/category/CategoryFragment.kt
+++ app/src/main/java/com/example/shoppingapp/ui/category/CategoryFragment.kt
@@ -1,32 +1,37 @@
 package com.example.shoppingapp.ui.category
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.example.shoppingapp.R
+import com.example.shoppingapp.databinding.FragmentCategoryBinding
 import com.example.shoppingapp.ui.common.ViewModelFactory
 
 class CategoryFragment : Fragment() {
 
     private val viewModel: CategoryViewModel by viewModels { ViewModelFactory(requireContext()) }
+    private lateinit var binding: FragmentCategoryBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_category, container, false)
+        binding = FragmentCategoryBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val categoryAdapter = CategoryAdapter()
+
+        binding.rvCategoryList.adapter = categoryAdapter
+
         viewModel.items.observe(viewLifecycleOwner) {
-            Log.d("CategoryFragment", "items=$it")
+            categoryAdapter.submitList(it)
         }
     }
 }
